cc6d330bb407a158bf0cb742f17f133d045cdb14
hotire <gngh0101@gmail.com>
2021-01-14 21:39:18 +0900

    Add requestParameter

--- src/main/java/com/github/hotire/spring/swagger/SwaggerConfig.java
+++ src/main/java/com/github/hotire/spring/swagger/SwaggerConfig.java
@@ -1,12 +1,15 @@
 package com.github.hotire.spring.swagger;
 
-import java.util.Collections;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
+import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.RequestParameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -15,10 +18,18 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).globalRequestParameters(Collections.emptyList())
+        return new Docket(DocumentationType.SWAGGER_2).globalRequestParameters(List.of(requestParameter()))
                                                       .select().apis(RequestHandlerSelectors.any())
                                                       .paths(PathSelectors.ant("/v1/**").or(PathSelectors.ant("/dev/**")))
                                                       .build();
 
+    }
+
+    public RequestParameter requestParameter() {
+        return new RequestParameterBuilder().name("token")
+                                            .in(ParameterType.HEADER)
+                                            .description("token")
+                                            .required(false)
+                                            .build();
     }
 }
