ac8ebef3d85bb6c2c047791615ce5795468dbab2
hotire <gngh0101@gmail.com>
2021-01-20 00:39:32 +0900

    Migrate parameter to requestParameter

--- src/main/java/com/github/hotire/spring/swagger/SwaggerConfig.java
+++ src/main/java/com/github/hotire/spring/swagger/SwaggerConfig.java
@@ -10,6 +10,7 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.RequestParameterBuilder;
 import springfox.documentation.schema.ModelRef;
+import springfox.documentation.schema.ScalarType;
 import springfox.documentation.service.Parameter;
 import springfox.documentation.service.ParameterType;
 import springfox.documentation.service.RequestParameter;
@@ -22,19 +23,20 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).globalRequestParameters(List.of(requestParameter()))
-                                                      .globalOperationParameters(List.of(parameter()))
                                                       .select()
                                                       .apis(RequestHandlerSelectors.any())
                                                       .paths(PathSelectors.ant("/v1/**").or(PathSelectors.ant("/dev/**")))
                                                       .build();
 
     }
-
+    
     public RequestParameter requestParameter() {
         return new RequestParameterBuilder().name("token")
                                             .in(ParameterType.HEADER)
                                             .description("token")
                                             .required(false)
+                                            .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING))
+                                                         .defaultValue("hello"))
                                             .build();
     }
 
